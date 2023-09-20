47d3076f9c765aca6c5b3710661a0d4aa03a4602
Honi2302 <129046156+Honi2302@users.noreply.github.com>
2023-09-20 19:54:45 +0530

    Add files via upload

---
+++ backend/db/config.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.connect(
+    "mongodb+srv://honey-sharma:Honey123@cluster0.g1eybso.mongodb.net/pharmacy?retryWrites=true&w=majority"
+);
+
